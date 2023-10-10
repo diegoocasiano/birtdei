125729f1aaacacf8daf3af3d038010cbf26a85c4
Diego Casiano <dianlonso02@gmail.com>
2023-10-10 12:46:55 -0500

    12:46 PM

--- birtdei.py
+++ birtdei.py
@@ -15,31 +15,40 @@ def birthday():
 def procesar_cumple():
     dia = int(request.form['dia'])
     mes = int(request.form['mes'])
+    anio = int(request.form['anio']) #2 ultimos digitos
+    
+    # Si los digitos del año que ingreso el usuario está en el rango de 0-49, se asume que se refiere a 2000
+    # Si los digitos del año que ingreso el usuario está en el rango de 50-99, se asume que se refiere a 1900
+    if anio >= 0 and anio <= 49:
+        anio_completo = 2000 + anio
+    else:
+        anio_completo = 2000 - (100 - anio)
 
     fecha_actual = datetime.now()
     dia_actual = fecha_actual.day
     mes_actual = fecha_actual.month
 
+    #Calcular la edad
+    edad = fecha_actual.year - anio_completo
+
     # Calcula la fecha de cumpleaños para este año
     fecha_cumple = datetime(fecha_actual.year, mes, dia)
-    print(fecha_cumple)
 
     # Usa el próximo año si ya pasó el cumpleaños
     if fecha_cumple < fecha_actual:
         fecha_cumple = datetime(fecha_actual.year + 1, mes, dia) 
     
-    # Calcula la diferencia en días hasta el cumpleaños
-    diferencia_dias = (fecha_cumple - fecha_actual).days
-    print(diferencia_dias)
-
+    # Calcula los días que faltan para el cumpleaños
+    dias_para_cumple = (fecha_cumple - fecha_actual).days
+    
     # Presentación de templates
     if dia == dia_actual and mes == mes_actual:
-        return render_template('s3.1.html')
+        return render_template('s3.1.html', edad=edad)
     
-    elif 0 <= diferencia_dias <= 31:
-        return render_template('s3.2.html')
+    elif 0 <= dias_para_cumple <= 31:
+        return render_template('s3.2.html', edad=edad, dias_para_cumple=dias_para_cumple)
     
-    elif 32 <= diferencia_dias <= 220:
+    elif 32 <= dias_para_cumple <= 220:
         return render_template('s3.3.html')
     
     else:
