89b1e4220be4b6a06cbd0542d24ed5373cc90d1b
Diego Casiano <dianlonso02@gmail.com>
2023-10-11 22:26:17 -0500

    design cards (with their content)

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Card from './components/Card'
 import NavBar from './components/shared/NavBar'
 function App() {
   return (
@@ -8,7 +9,18 @@ function App() {
       </header>
 
       <main>
-          
+          <div className="main-cards-container">
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+          </div>
       </main>
     </>
     
--- react-components/src/components/Card.jsx
+++ react-components/src/components/Card.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 import './card.css'
 
 function Card() {
+  const ArrowRightIcon = '/public/arrow-right-card.svg'
+  const GiftBrand = '/public/gift-card.svg'
+
   return (
-    <div className='mc-card'>Soy una card</div>
+    <div className="card-container">
+      <div className='card-logo-marca'></div>
+      <div className='card-content-container'>
+        <h1 className='h1-regalo-marca'> <img src={GiftBrand} />1 Helado</h1>
+        <p className='nombre-marca'>McDonald's</p>
+        <p className='categoria-marca'>Fast Food</p>
+        <button className='btn-detalles-regalo'>Más detalles <img src={ArrowRightIcon} className='arrow-icon'/> </button>
+      </div>
+    </div>
   )
 }
 
