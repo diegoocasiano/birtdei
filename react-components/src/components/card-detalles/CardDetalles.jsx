a62269a780c48a616a7c926a63358dfd325ff1cb
Diego Casiano <dianlonso02@gmail.com>
2023-10-17 01:31:04 -0500

    01:30 - 17/10/23

--- react-components/src/components/card-detalles/CardDetalles.jsx
+++ react-components/src/components/card-detalles/CardDetalles.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useRef, useState} from 'react';
 import './card-detalles.css'
 
 function CardDetalles() {
@@ -6,27 +6,39 @@ function CardDetalles() {
     const ImportantIcon = '/public/important-icon.svg'
 
     const [numeroCondiciones, setNumeroCondiciones] = useState(0);
+    const [numeroSaltosDeLinea, setNumeroSaltosDeLinea] = useState(0);
+    const paragraphRef = useRef(null);
 
      // Aumentar el height de detallesVentana cuando aumenta el numero de condiciones
     useEffect(() => {
         const condiciones = document.querySelectorAll('.condicion');
         setNumeroCondiciones(condiciones.length);
 
-    }, [numeroCondiciones]); // Ejecutar cuando el número de condiciones cambie
+    }, [numeroCondiciones]); // Ejecutar cuando el número de condiciones cambie - Cuando se abra el menu (ahi numeroCondiciones tendrá un valor)
 
-    const calcularAltoSection = (numeroCondiciones) => {
-        return 280 + (numeroCondiciones * 20) + 'px';
-    }
-    
+    useEffect(() => {
+        const paragraph = paragraphRef.current;
+        if (paragraph) {
+          const lines = paragraph.clientHeight / 18;
+          setNumeroSaltosDeLinea(Math.round(lines) - numeroCondiciones ); // Math.ceil es para redondear para arriba el valor de (lines)
+        }
+      }, [numeroCondiciones]); // Ejecutar cuando el número de condiciones cambie - Cuando se abra el menu (ahi numeroCondiciones tendrá un valor)
+
+      const calcularAltoSection = (numeroCondiciones, numeroSaltosDeLinea) => {
+        return 280 + (numeroCondiciones * 20) + (numeroSaltosDeLinea * 18) + 'px';
+      };
+
 
   return (
     <>
-        <section className="card-detalles-container" style={{ height: calcularAltoSection(numeroCondiciones) }}>
+        <section className="card-detalles-container" style={{ height: calcularAltoSection(numeroCondiciones, numeroSaltosDeLinea) }}>
             <div className="marca-info">
                 <div className="logo-container">
-                    <img src="" alt="" />
+                    <img src="" />
                 </div>
-                <h1>McDonald's - N° saltoLineas: </h1>
+                <h1>McDonald's</h1>
+                {/* <h1>N° saltosLineas: {numeroSaltosDeLinea}</h1>
+                <p> N° condiciones: {numeroCondiciones}</p> */}
             </div>
 
             <hr className="line-top" />
@@ -36,24 +48,19 @@ function CardDetalles() {
                     <p>Te regala</p>
                     <h1 className='h1-regalo-marca'><img src={GiftBrand} />1 Helado</h1>
                 </div>
-                <div className="condiciones-main-container">
+                <div className="condiciones-main-container" >
                     <h2>Condiciones</h2>
-                    <div className="condiciones-container">
+                    <div className="condiciones-container" ref={paragraphRef} >
                         <div className="condicion">
                             <div className="bullet"></div>
                             <p className='line-txt'>Presentar DNI</p>
                         </div>
                         <div className="condicion">
                             <div className="bullet"></div>
-                            <p className='txt'>
-                            primera linea
-                            <br />
-                            segunda linea
-                            <br />
-                            tercera linea
-                            </p>
+                            <p className='line-txt'>Presentar DNI</p>
                         </div>
-                        
+                       
+                                  
                     </div>
                 </div>
                 <div className="msj-important">
