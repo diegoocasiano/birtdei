b1f4c72e0878d57c4e2490987b66396aeedd652a
Diego Casiano <dianlonso02@gmail.com>
2023-11-03 21:47:45 -0500

    prueba5

--- birtdei.py
+++ birtdei.py
@@ -26,14 +26,6 @@ def inicio():
     else:
         return render_template('index1.html')
 
-@app.route('/index1')
-def index1():
-    return render_template("index1.html")
-
-@app.route('/index2')
-def index2():
-    return render_template("index2.html")
-
 
 @app.route('/ingreso', methods=['POST'])
 def procesar_cumple():
